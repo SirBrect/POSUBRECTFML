ab0dbc27c30e2ac8340d5c104e9fe24daa65d2b4
SirBrect <Kerrab@rpi.edu>
2018-12-09 22:08:44 -0500

    argument checking

--- p1.cpp
+++ p1.cpp
@@ -9,15 +9,17 @@ static int id;
 int main(int argc, char const *argv[])
 {
 	// var dicks-------------------------------------------------------------
-	std::commands;
-	bool forwarding = (*argv[1] == 'F');
+	std::vector<Commands> commanLines; //holds command objects for as many lines
+	bool forwarding = (*argv[1] == 'F'); //bool determinig forwording
 	if (argc > 3){
-		std::cerr << "Invalid arguments" << std::endl
+		std::cerr << "Invalid Arguments PAT!" << std::endl;
 	}
-
-
-
 	std::ifstream mipscode(argv[2]);
+	if (!mipscode.good())
+	{
+		std::cerr << "Cannot Read FILE PAT!" << std::endl;
+	}
+	
 
 
 
